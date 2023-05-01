d14b8cf5d3ea81d2b6796d9bf664b7136b03ac7d
elisa-sh <126708543+elisa-sh@users.noreply.github.com>
2023-05-01 17:55:49 -0400

    completed 75%of the problem

--- lib/dog.py
+++ lib/dog.py
@@ -12,4 +12,43 @@ APPROVED_BREEDS = [
 ]
 
 class Dog:
-    pass
+    def __init__(self, name):
+        self._name = name
+
+    def get_name(self):
+        return self._name
+
+    def set_name(self, name):
+        if type(name) == str and 1 <= len(name) <= 25:
+            self._name = name.title()
+        else:
+            print("Name must be string between 1 and 25 characters.")
+
+    name = property(get_name, set_name)
+
+    # def __init__(self, name):
+    #     self._name = name
+
+    # def get_name(self):
+    #     return self._name
+    
+    # def set_name(self, name):
+    #     if (type(name) in (str) and (1<= name <= 25)):
+    #         print(self.name)
+    #     else:
+    #         print("Name must be string between 1 and 25 characters.")
+    # name = property(set_name, get_name)
+
+    # def __init__(self, name):
+    #     self._name = name
+    
+    # @property
+    # def name(self):
+    #     return self._name
+    
+    # @name.setter
+    # def name(self, value):
+    #     if not isinstance(value, str) or not 1 <= len(value) <= 25:
+    #         print("Name must be string between 1 and 25 characters.")
+    #     else:
+    #         self._name = value
